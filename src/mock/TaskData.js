9cc8cd367bd55f3fecac962052a12818541fc950
techstackmedia <osagienoah04@gmail.com>
2022-06-19 22:24:27 +0100

    add mock image data

--- src/mock/TaskData.js
+++ src/mock/TaskData.js
@@ -6,7 +6,8 @@ const TaskData = [
     price: "N500.00",
     delivery: "3",
     assigned: "Adamu Musa",
-    img: "https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1644990728693%2FWHHblb4Jn.jpeg%3Fw%3D150%26h%3D150%26fit%3Dcrop%26crop%3Dfaces%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
+    image:
+      "https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1644990728693%2FWHHblb4Jn.jpeg%3Fw%3D150%26h%3D150%26fit%3Dcrop%26crop%3Dfaces%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
   },
   {
     id: 2,
@@ -15,6 +16,8 @@ const TaskData = [
     price: "N500.00",
     delivery: "3",
     assigned: "Jerry Pet",
+    image:
+      "https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1597741164753%2FothOPYGRR.jpeg%3Fw%3D150%26h%3D150%26fit%3Dcrop%26crop%3Dfaces%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
   },
   {
     id: 3,
@@ -23,6 +26,8 @@ const TaskData = [
     price: "N500.00",
     delivery: "3",
     assigned: "Adamu Musa",
+    image:
+      "https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1597741164753%2FothOPYGRR.jpeg%3Fw%3D150%26h%3D150%26fit%3Dcrop%26crop%3Dfaces%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
   },
   {
     id: 4,
@@ -31,6 +36,8 @@ const TaskData = [
     price: "N500.00",
     delivery: "3",
     assigned: "Adamu Musa",
+    image:
+      "https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1604253507198%2FQeNYXv7mn.jpeg%3Fw%3D150%26h%3D150%26fit%3Dcrop%26crop%3Dfaces%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
   },
   {
     id: 5,
@@ -39,6 +46,8 @@ const TaskData = [
     price: "N500.00",
     delivery: "3",
     assigned: "Adamu Musa",
+    image:
+      "https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1612672474877%2F3T2K2h7zw.jpeg%3Fw%3D150%26h%3D150%26fit%3Dcrop%26crop%3Dfaces%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
   },
   {
     id: 6,
@@ -47,6 +56,8 @@ const TaskData = [
     price: "N500.00",
     delivery: "3",
     assigned: "Adamu Musa",
+    image:
+      "https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1622006419578%2FwJIH-HgrQ.png%3Fw%3D150%26h%3D150%26fit%3Dcrop%26crop%3Dfaces%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
   },
 ];
 
